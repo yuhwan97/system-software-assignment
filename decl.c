2f97d2fed5903dc776899ed33164661fa683ffb4
yuhwan97 <yuhwan9706@gmail.com>
2019-10-01 14:32:19 +0900

    good

---
+++ decl.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters */
+/* We do not support C11 <threads.h>.  */
+ {"bitOr", (funct_t) bitOr, (funct_t) test_bitOr, 2, "& ~", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isEqual", (funct_t) isEqual, (funct_t) test_isEqual, 2,
+    "! ~ & ^ | + << >>", 5, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"anyEvenBit", (funct_t) anyEvenBit, (funct_t) test_anyEvenBit, 1,
+    "! ~ & ^ | + << >>", 12, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"allEvenBits", (funct_t) allEvenBits, (funct_t) test_allEvenBits, 1,
+    "! ~ & ^ | + << >>", 12, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"rotateLeft", (funct_t) rotateLeft, (funct_t) test_rotateLeft,
+   2, "! ~ & ^ | + << >>", 25, 3,
+  {{TMin, TMax},{0,31},{TMin,TMax}}},
+ {"greatestBitPos", (funct_t) greatestBitPos, (funct_t) test_greatestBitPos, 1, "! ~ & ^ | + << >>", 70, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isTmax", (funct_t) isTmax, (funct_t) test_isTmax, 1, "! ~ & ^ | +", 10, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"leastBitPos", (funct_t) leastBitPos, (funct_t) test_leastBitPos, 1, "! ~ & ^ | + << >>", 6, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"subOK", (funct_t) subOK, (funct_t) test_subOK, 2,
+    "! ~ & ^ | + << >>", 20, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isLessOrEqual", (funct_t) isLessOrEqual, (funct_t) test_isLessOrEqual, 2,
+    "! ~ & ^ | + << >>", 24, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"satMul3", (funct_t) satMul3, (funct_t) test_satMul3, 1,
+    "~ & ^ | + << >>", 25, 3,
+  {{TMin,TMax},{TMin,TMax},{TMin,TMax}}},
+ {"divpwr2", (funct_t) divpwr2, (funct_t) test_divpwr2, 2,
+    "! ~ & ^ | + << >>", 15, 2,
+  {{TMin, TMax},{0,30},{TMin,TMax}}},
+ {"float_abs", (funct_t) float_abs, (funct_t) test_float_abs, 1,
+    "$", 10, 2,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_half", (funct_t) float_half, (funct_t) test_float_half, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_i2f", (funct_t) float_i2f, (funct_t) test_float_i2f, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
---
+++ tests.c
@@ -0,0 +1,160 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters */
+/* We do not support C11 <threads.h>.  */
+int test_bitOr(int x, int y)
+{
+  return x|y;
+}
+int test_isEqual(int x, int y)
+{
+  return x == y;
+}
+int test_anyEvenBit(int x) {
+  int i;
+  for (i = 0; i < 32; i+=2)
+      if (x & (1<<i))
+   return 1;
+  return 0;
+}
+int test_allEvenBits(int x) {
+  int i;
+  for (i = 0; i < 32; i+=2)
+      if ((x & (1<<i)) == 0)
+   return 0;
+  return 1;
+}
+int test_rotateLeft(int x, int n) {
+  unsigned u = (unsigned) x;
+  int i;
+  for (i = 0; i < n; i++) {
+      unsigned msb = u >> 31;
+      unsigned rest = u << 1;
+      u = rest | msb;
+  }
+  return (int) u;
+}
+int test_greatestBitPos(int x) {
+    unsigned mask = 1<<31;
+    if (x == 0)
+ return 0;
+    while (!(mask & x)) {
+ mask = mask >> 1;
+    }
+    return mask;
+}
+int test_isTmax(int x) {
+    return x == 0x7FFFFFFF;
+}
+int test_leastBitPos(int x) {
+  int mask = 1;
+  if (x == 0)
+    return 0;
+  while (!(mask & x)) {
+    mask = mask << 1;
+  }
+  return mask;
+}
+int test_subOK(int x, int y)
+{
+  long long ldiff = (long long) x - y;
+  return ldiff == (int) ldiff;
+}
+int test_isLessOrEqual(int x, int y)
+{
+  return x <= y;
+}
+int test_satMul3(int x)
+{
+  if ((x+x+x)/3 != x)
+    return x < 0 ? 0x80000000 : 0x7FFFFFFF;
+  else
+    return 3*x;
+}
+int test_divpwr2(int x, int n)
+{
+    int p2n = 1<<n;
+    return x/p2n;
+}
+unsigned test_float_abs(unsigned uf) {
+  float f = u2f(uf);
+  unsigned unf = f2u(-f);
+  if (isnan(f))
+    return uf;
+  /* An unfortunate hack to get around a limitation of the BDD Checker */
+  if ((int) uf < 0)
+      return unf;
+  else
+      return uf;
+}
+unsigned test_float_half(unsigned uf) {
+  float f = u2f(uf);
+  float hf = 0.5*f;
+  if (isnan(f))
+    return uf;
+  else
+    return f2u(hf);
+}
+unsigned test_float_i2f(int x) {
+  float f = (float) x;
+  return f2u(f);
+}
